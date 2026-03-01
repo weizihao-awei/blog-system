b98d43759233005357b1e345419d81772e8d8b4d
伟字号 <yekunwei@hcrc1.wecom.work>
2026-03-01 14:32:13 +0800

    refactor(config): 将配置文件从properties格式转换为yaml格式并调整端口
    - 将application.properties重命名为application.properties.yaml
    - 将配置从properties格式转换为yaml格式
    - 服务器端口从8081修改为8089
    - 移除HelloController返回字符串中的多余空格

--- src/main/java/com/ykw/blog_system/HelloController.java
+++ src/main/java/com/ykw/blog_system/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     @RequestMapping("/hello") //标识请求路径
     public String hello(String name){
         System.out.println("HelloController ... hello: " + name);
-        return "Hello ss" + name;
+        return "Hello " + name;
     }
 
 }
